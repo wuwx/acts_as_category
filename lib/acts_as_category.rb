c0717e620e60abf64fd4195f0bd6cbdebdac719d
wuwx <wuweixin@gmail.com>
2013-03-26 19:35:01 +0800

    used_by and used_by?

--- lib/acts_as_category.rb
+++ lib/acts_as_category.rb
@@ -9,21 +9,43 @@ module ActsAsCategory
   module ClassMethods
     def acts_as_category
       class_eval <<-EOV
+      
         belongs_to :resource, :polymorphic => true
-        scope :used_by, lambda { |resource| where(:resource_type => resource) }
+        
+        def self.used_by(*resources)
+          context = resource_id = resource_type = nil
+          resources.each do |resource|
+            if resource.kind_of?(ActiveRecord::Base)
+              resource_id   ||= resource.id
+              resource_type ||= resource.class
+            else
+              context ||= resource
+            end
+          end
+          where(:context => context, :resource_id => resource_id, :resource_type => resource_type)
+        end
+        
         include InstanceMethods
+        
       EOV
     end
   end
 
   module InstanceMethods
-    def used_by?(resource)
-      if resource.is_a?(Class)
-        self.class.where(:resource_type => resource).size > 0
-      else
-        self.class.where(:resource_id => resource, :resource_type => resource.class).size > 0
+    
+    def used_by?(*resources)
+      context = resource_id = resource_type = nil
+      resources.each do |resource|
+        if resource.kind_of?(ActiveRecord::Base)
+          resource_id   ||= resource.id
+          resource_type ||= resource.class
+        else
+          context ||= resource
+        end
       end
+      self.class.where(:context => context, :resource_id => resource_id, :resource_type => resource_type).size > 0
     end
+    
   end
 
 end
